df2313e3962a467aaee944b2bc1eda4ba67e9b07
aliadelelroby <aliadelelroby@gmail.com>
2021-12-31 01:12:34 +0200

    Some Changes

--- components/layouts/index.tsx
+++ components/layouts/index.tsx
@@ -1,21 +1,23 @@
 import Cursor from "components/global/Cursor";
 import Navbar from "components/global/Navbar";
+import { NextRouter } from "next/dist/client/router";
 import Head from "next/head";
+import { ReactNode } from "react";
 // Types
 type LayoutProps = {
-    router: {};
-    children: any;
+	router: NextRouter;
+	children: ReactNode;
 };
 
 export const Main = ({ router, children }: LayoutProps) => {
-    return (
-        <main>
-            <Head>
-                <link rel="icon" href="/favicon.png" />
-            </Head>
-            <Cursor />
-            <Navbar router={router} />
-            {children}
-        </main>
-    );
+	return (
+		<main>
+			<Head>
+				<link rel="icon" href="/favicon.png" />
+			</Head>
+			<Cursor />
+			<Navbar router={router} />
+			{children}
+		</main>
+	);
 };
